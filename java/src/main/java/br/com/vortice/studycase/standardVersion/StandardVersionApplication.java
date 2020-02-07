cf03e31b6072e3a80cb004721523113bde232280
Antonio Amorim <afamorim@gmail.com>
2020-02-07 17:50:22 -0300

    feat(project): build java project

---
+++ java/src/main/java/br/com/vortice/studycase/standardVersion/StandardVersionApplication.java
@@ -0,0 +1,13 @@
+package br.com.vortice.studycase.standardVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardVersionApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/br/com/vortice/studycase/standardVersion/StandardVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vortice.studycase.standardVersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
